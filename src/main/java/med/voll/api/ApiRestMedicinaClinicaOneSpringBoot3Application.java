cc2a792bb069a20889fe31bceee57820ac66f18e
Joel Fiaré <joelfiare@gmail.com>
2023-09-10 20:11:38 -0300

    first commit

---
+++ src/main/java/med/voll/api/ApiRestMedicinaClinicaOneSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package med.voll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMedicinaClinicaOneSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMedicinaClinicaOneSpringBoot3Application.class, args);
+	}
+
+}
