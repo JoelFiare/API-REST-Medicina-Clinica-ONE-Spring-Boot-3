6a2ec84eced0be9182bd49c83d6f59a33583a1e9
Joel Fiaré <joelfiare@gmail.com>
2023-09-17 22:22:34 -0300

    Cafe Babe ;)

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,18 +1,13 @@
 package med.voll.api.controller;
 
 import med.voll.api.paciente.DatosRegistroPaciente;
-import med.voll.api.paciente.PacienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
-    @Autowired
-    private PacienteRepository repository;
+    //@Autowired
+    //private PacienteRepository repository;
 
     @PostMapping
     public void registrar(@RequestBody DatosRegistroPaciente datos) {
--- src/main/java/med/voll/api/paciente/PacienteRepository.java
+++
@@ -1,8 +0,0 @@
-package med.voll.api.paciente;
-
-import med.voll.api.medico.Medico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-
-}
