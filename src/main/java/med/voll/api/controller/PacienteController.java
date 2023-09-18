c462a1125809939ccb62b0c453631e252084d0ea
Joel Fiaré <joelfiare@gmail.com>
2023-09-17 21:09:39 -0300

    Cafe Babe ;)

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,18 +1,28 @@
 package med.voll.api.controller;
 
 import med.voll.api.paciente.DatosRegistroPaciente;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("pacientes")
+@RequestMapping("/pacientes")
 public class PacienteController {
+    @Autowired
+    private PacienteRepository repository;
 
     @PostMapping
     public void registrar(@RequestBody DatosRegistroPaciente datos) {
         System.out.println("datos recebidos: " +datos);
     }
+
+    //TODO
+    //@GetMapping
+    //public Page<DatosListaPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nombre"}) Pageable paginacion {
+    //    return repository.findAll(paginacion).map(DatosListaPaciente::new);
+    //}
 
 }
