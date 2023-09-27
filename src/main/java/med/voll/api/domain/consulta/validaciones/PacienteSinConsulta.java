1bf183d603fa0aa7eb5a207f4a1979bcade209bc
Joel Fiaré <joelfiare@gmail.com>
2023-09-27 11:12:42 -0300

    Principios SOLID

--- src/main/java/med/voll/api/domain/consulta/validaciones/PacienteSinConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/validaciones/PacienteSinConsulta.java
@@ -8,15 +8,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PacienteSinConsulta implements ValidadorDeConsultas{
+
     @Autowired
     private ConsultaRepository repository;
-    public void validar(DatosAgendarConsulta datos){
+
+    public void validar(DatosAgendarConsulta datos)  {
         var primerHorario = datos.fecha().withHour(7);
-        var ultimoHorario = datos.fecha().withHour(18);
-        var pacienteConConsulta = repository.existByPacienteIdAndDataBetween(datos.idPaciente(),primerHorario,ultimoHorario);
+        var ultimoHorario= datos.fecha().withHour(18);
+
+        var pacienteConConsulta=repository.existsByPacienteIdAndFechaBetween(datos.idPaciente(),primerHorario,ultimoHorario);
 
         if(pacienteConConsulta){
             throw new ValidationException("el paciente ya tiene una consulta para ese dia");
         }
+
     }
 }
