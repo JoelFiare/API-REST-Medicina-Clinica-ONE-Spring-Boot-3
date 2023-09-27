7b34cf24846ec24e33ec0bb9646b7f4d8891eecb
Joel Fiaré <joelfiare@gmail.com>
2023-09-27 10:43:55 -0300

    Reglas de negocio

---
+++ src/main/java/med/voll/api/domain/consulta/validaciones/PacienteSinConsulta.java
@@ -0,0 +1,18 @@
+package med.voll.api.domain.consulta.validaciones;
+
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DatosAgendarConsulta;
+
+public class PacienteSinConsulta {
+    private ConsultaRepository repository;
+    public void validar(DatosAgendarConsulta datos){
+        var primerHorario = datos.fecha().withHour(7);
+        var ultimoHorario = datos.fecha().withHour(18);
+        var pacienteConConsulta = repository.existByPacienteIdAndDataBetween(datos.idPaciente(),primerHorario,ultimoHorario);
+
+        if(pacienteConConsulta){
+            throw new ValidationException("el paciente ya tiene una consulta para ese dia");
+        }
+    }
+}
